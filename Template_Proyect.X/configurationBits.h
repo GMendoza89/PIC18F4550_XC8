0110fcbad4a8330ccd127f2b595596c44ea8e1fb
Gustavo Mendoza <37191104+GMendoza89@users.noreply.github.com>
2024-06-26 11:35:40 -0600

    Se incorpora plantilla base de los proyectos, considerando un oscilador externo de 20 MHz

---
+++ Template_Proyect.X/configurationBits.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   configurationBits.h
+ * Author: Ing. Gustavo David Mendoza Pinto
+ *
+ * Created on March 21, 2024, 7:34 PM
+ */
+
+#ifndef CONFIGURATIONBITS_H
+#define	CONFIGURATIONBITS_H
+
+#include<xc.h>
+
+#define __XTAL_FREQ 20000000
+#define true 1
+#define false 0
+
+#endif	/* CONFIGURATIONBITS_H */
+
---
+++ Template_Proyect.X/main.c
@@ -0,0 +1,23 @@
+/*
+ * File:   main.c
+ * Author: gustavomendoza
+ *
+ * Created on March 21, 2024, 7:35 PM
+ */
+
+
+#include <xc.h>
+#include "configurationBits.h"
+
+void main(void) {
+    // Setup code here
+    
+    while(true){
+        // Loop code here
+        
+        
+        
+    }
+    
+    return;
+}
